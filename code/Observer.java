3ada7caa3b2b5ac2043081ae1c51aa182bc61399
Eric Zimmer <eric.zimmer@mymail.champlain.edu>
2021-04-06 08:51:52 -0600

    Create Eclipse project, remove outdated tests

--- code/Observer.java
+++
@@ -1,9 +0,0 @@
-package code;
-// Eric Zimmer, Laurence Madsen
-// eric.zimmer@mymail.champlain.edu, laurence.madsen@mymail.champlain.edu
-// Final Project
-
-// Abstract class to be implemented by objects seeking to track BankAccount status
-public abstract class Observer {
-	public abstract void update(double balance);
-}
